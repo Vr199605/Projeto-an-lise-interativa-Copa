5d32cadc7e66bd6d0aac0f32c5b965253bd3aee2
Vr199605 <victormoreiraicnv@gmail.com>
2026-06-24 14:31:20 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import TerminalPreditivo from './TerminalPreditivo.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TerminalPreditivo />
+  </StrictMode>
+)
